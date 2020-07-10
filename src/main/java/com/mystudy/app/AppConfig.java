f32324d06e945b74cff08ee92a8bb7d869a84f10
wangjiteng <18963603500@189.cn>
2020-07-10 16:05:21 +0800

    添加json支持

--- src/main/java/com/mystudy/app/AppConfig.java
+++ src/main/java/com/mystudy/app/AppConfig.java
@@ -1,9 +1,34 @@
 package com.mystudy.app;
 
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @ComponentScan({"com.mystudy.controller","com.mystudy.entity"})
-public class AppConfig {
+@EnableWebMvc
+public class AppConfig implements WebMvcConfigurer {
+    /**
+        配置json转换器
+     **/
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
+        converters.add(fastJsonHttpMessageConverter);
+    }
+
+    /**
+        配置视图解析器
+     **/
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp();
+    }
 }
--- src/main/java/com/mystudy/controller/OrderController.java
+++ src/main/java/com/mystudy/controller/OrderController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 public class OrderController {
     @RequestMapping("/buyCar")
@@ -11,5 +14,14 @@ public class OrderController {
     public String buyCar() {
         System.out.println("========buyCar========");
         return "suuccess";
+    }
+
+    @RequestMapping("/buyCar2")
+    @ResponseBody
+    public Map<String,Object> buyCar2() {
+        System.out.println("========buyCar2========");
+        Map<String,Object> map = new HashMap<String,Object>();
+        map.put("key", "value");
+        return map;
     }
 }
